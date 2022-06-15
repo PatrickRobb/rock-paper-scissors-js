c3bd834e53fccdd89d0d91234050a3a93aed49b1
Patrick Robb <patrickrobb1997@gmail.com>
2022-06-15 13:28:47 -0400

    added basic button gui for playing and updating the score.

--- index.js
+++ index.js
@@ -1,47 +1,92 @@
-let humanSubmission = window.prompt("Please choose Rock, Paper, or Scissors: ").toLowerCase();
-while(humanSubmission !== "rock" && humanSubmission !== "paper" && humanSubmission !== "scissors"){
-	humanSubmission = window.prompt("Your did not make a valid selection. Please try again: ").toLowerCase();
-}
+// let humanSubmission = window.prompt("Please choose Rock, Paper, or Scissors: ").toLowerCase();
+// while(humanSubmission !== "rock" && humanSubmission !== "paper" && humanSubmission !== "scissors"){
+// 	humanSubmission = window.prompt("Your did not make a valid selection. Please try again: ").toLowerCase();
+// }
 
-console.log(game(humanSubmission, computerPlay()));
+let playerScore = 0;
+let computerScore = 0;
+
+const score = document.createElement('h1');
+const body = document.querySelector('body')
+body.appendChild(score);
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', (e) => {
+	game(e.target.textContent);
+});	
+
+paper.addEventListener('click', (e) => {
+	game(e.target.textContent);
+});
+
+scissors.addEventListener('click', (e) => {
+	game(e.target.textContent);
+});
 
 function computerPlay(){
 	let randomNum = Math.random() *3;
 	if (randomNum < 1){
-		return "rock";
+		return "Rock";
 	} else if (randomNum < 2){
-		return "paper";
-	} else return "scissors";
+		return "Paper";
+	} else return "Scissors";
 }
 
-function game(humanPlay, computerPlay){
+function game(humanPlay){
 	let human = humanPlay;
-	let computer = computerPlay;
+	let computer = computerPlay();
 	if (human === computer){
-		return(`Draw! You played ${human}, and the computer played ${computer}.`);
+		score.textContent = `Draw! You played ${human}, and the computer played ${computer}. The score is computer: 
+		${computerScore} and player: ${playerScore}`;
 	}
-	else if (human === "rock"){
-		if (computer === "paper"){
-			return(`You lose! You played ${human}, and the computer played ${computer}.`);
+	else if (human === "Rock"){
+		if (computer === "Paper"){
+			computerScore++;
+			winCondition();
+			score.textContent = `You lose! You played ${human}, and the computer played ${computer}. The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 		else {
-			return(`Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide!`);
+			playerScore++;
+			winCondition();
+			score.textContent = `Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide! The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 	}
-	else if (human === "paper"){
-		if (computer === "scissors"){
-			return(`You lose! You played ${human}, and the computer played ${computer}.`);
+	else if (human === "Paper"){
+		if (computer === "Scissors"){
+			computerScore++;
+			winCondition();
+			score.textContent = `You lose! You played ${human}, and the computer played ${computer}. The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 		else {
-			return(`Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide!`);
+			playerScore++;
+			winCondition();
+			score.textContent = `Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide! The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 	}
-	else if (human === "scissors"){
-		if (computer === "rock"){
-			return(`You lose! You played ${human}, and the computer played ${computer}.`);
+	else if (human === "Scissors"){
+		if (computer === "Rock"){
+			computerScore++;
+			winCondition();
+			score.textContent = `You lose! You played ${human}, and the computer played ${computer}. The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 		else {
-			return(`Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide!`);
+			playerScore++;
+			winCondition();
+			score.textContent = `Victoire! You played ${human}, and the computer played ${computer}. A bien tot, ordinateur stupide! The score is computer: 
+			${computerScore} and player: ${playerScore}`;
 		}
 	}
+}
+
+function winCondition(){
+	if (playerScore >= 5) alert("Human player wins!!!");
+	else if (computerScore >= 5) alert ("Computer player wins!!!");
 }
